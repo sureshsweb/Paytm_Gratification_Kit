4da0fec6ebfc61023d3d5f7fb0207579a5238495
Paytm-Payments <Mohit.aggarwal@paytm.com>
2017-02-03 18:40:51 +0530

    java Kit submission

---
+++ JAVA/src/main/java/com/paytm/wallet/gratification/App.java
@@ -0,0 +1,13 @@
+package com.paytm.wallet.gratification;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
