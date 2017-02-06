5c3a50ef5f27178a1ce28d65b37eec455f852924
vidisha-goel <vidisha.goel@paytm.com>
2017-02-06 13:30:55 +0530

    Update test.py

--- PYTHON/python 3.X/test.py
+++ PYTHON/python 3.X/test.py
@@ -11,16 +11,16 @@ import pycurl
 
 applicationType="application/json"
 currentTime= int(round(time.time() * 1000))
-MERCHANT_KEY = 'kbzk1DSbJiV_O3p5';
+MERCHANT_KEY = 'xxxxxxxxxxxxxxxxxx';
 nonce=currentTime
 
-requestData1=OrderedDict([("request",OrderedDict([("requestType","VERIFY"),("merchantGuid","813FA430-038A-11E5-AADC-3CD92BEE4FB0"),("merchantOrderId","12121236666"),("salesWalletName", None),("salesWalletGuid","5E22F85F-AC39-47A5-9EBA-585B54B66623"),("payeeEmailId", None),("payeePhoneNumber","9711139557"),("payeeSsoId",""),("appliedToNewUsers","Y"),("amount","1"),("currencyCode","INR")])),("metadata","Testing Data"),("ipAddress","127.0.0.1"),("platformName","PayTM"),("operationType","SALES_TO_USER_CREDIT")])
+requestData1=OrderedDict([("request",OrderedDict([("requestType","VERIFY"),("merchantGuid","XXXXXXXXXXXXXXXXXXXXXXXXXXX"),("merchantOrderId","12121236666"),("salesWalletName", None),("salesWalletGuid","XXXXXXXXXXXXXXXXXXXXXXXXXXX"),("payeeEmailId", None),("payeePhoneNumber","9711139557"),("payeeSsoId",""),("appliedToNewUsers","Y"),("amount","1"),("currencyCode","INR")])),("metadata","Testing Data"),("ipAddress","127.0.0.1"),("platformName","PayTM"),("operationType","SALES_TO_USER_CREDIT")])
 requestData = json.dumps(requestData1, separators=(',', ':'))
 
 checksum = Checksum.generate_checksum_by_str(requestData1, MERCHANT_KEY)
 hmacHeaders = {
         'Content-Type':'application/json',
-        'mid':'813FA430-038A-11E5-AADC-3CD92BEE4FB0',
+        'mid':'XXXXXXXXXXXXXXXXXXXXXXXXXXX',
         'checksumhash' : checksum
    	 }
 
